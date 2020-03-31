a4b8f61499aa5b97406d07337f6704abe5cc3023
spandey <spandey@deerwalk.com>
2020-03-31 14:02:02 +0545

    router vs browserrouter

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './todos/Dashboard'; // added
 import Header from './layout/Header'; // added
-import { Router, Route, Switch } from 'react-router-dom'; // added
+import {Router, Route, Switch } from 'react-router-dom'; // added
+
+//Browser router doesn't require the attribute history to be associated with it
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // added
 
 import history from '../history'; // added
 import TodoDelete from './todos/TodoDelete'; // added
@@ -17,7 +20,8 @@ class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Router history={history}>
+                <!-- <Router> -->
+                <Router history={history} >
                     <Header />
                     <Switch>
                         <Route exact path='/' component={Dashboard} />
